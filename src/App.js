c5d3550fb3ab1315c7412a875f3de56b377aef3c
wmc2tokyo <wmcooper2@yahoo.com>
2019-11-06 20:20:10 +0900

    Started working on static outline. Almost finished with the search bar.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Search from "./components/search";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button type="button" class="btn btn-primary">
-          Primary
-        </button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search />
     </div>
   );
 }
