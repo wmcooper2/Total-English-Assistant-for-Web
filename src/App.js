3f9e29de0d5b7c074ce736268a7fa2eff6f2824a
wmc2tokyo <wmcooper2@yahoo.com>
2019-11-09 18:43:26 +0900

    Got the results to render as the input is changed. Results show top ten, but do not disappear when the input is cleared.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Result from "./components/results";
 import Search from "./components/search";
 import Dictionary from "./totalenglishdictionary.js";
 import levenshteinDistance from "./borrowed/levenshtein.js";
-import { tsOptionalType } from "@babel/types";
 
 const ENTRIES = Object.getOwnPropertyNames(Dictionary).sort();
 
@@ -42,22 +41,25 @@ class App extends React.Component {
     console.log(this.state.buttons[props - 1].toggled);
   };
 
-  handleInput = props => {
-    //update the searchWord for each key press
-    this.setState({
-      searchWord: props
-    });
-
-    //search for matching word as user types
-    let levenshteinResults = [];
+  levenshtein = () => {
+    let temp = [];
     for (let entry of ENTRIES) {
       let result = {
         searchResult: entry,
         value: levenshteinDistance(this.state.searchWord, entry)
       };
-      levenshteinResults.push(result);
+      temp.push(result);
     }
+    return temp;
+  };
 
+  handleInput = props => {
+    //search for matching word as user types
+    this.setState({
+      searchWord: props
+    });
+
+    let simpleResults = this.levenshtein();
     let topTenish = [];
     let topEntries = [];
     if (Dictionary[props]) {
@@ -65,30 +67,29 @@ class App extends React.Component {
       this.setState({
         results: Dictionary[props]
       });
-      let allResults = levenshteinResults.sort((a, b) => a.value - b.value);
-      topTenish = allResults.slice(0, 9);
-      //extract all the entries from the Dictionary
+      topTenish = simpleResults.sort((a, b) => a.value - b.value).slice(0, 9);
       topEntries.push({
         //the exact match
         word: props,
         entry: Dictionary[props]
       });
-      for (let result of topTenish) {
-        //all the others
-        topEntries.push({
-          word: result.searchResult,
-          entry: Dictionary[result.searchResult]
-        });
-      }
-      console.log(topEntries);
     } else {
-      //needed to clear the pill when exact match not found
-      let allResults = levenshteinResults.sort((a, b) => a.value - b.value);
-      topTenish = allResults.slice(0, 10);
+      topTenish = simpleResults.sort((a, b) => a.value - b.value).slice(0, 10);
       this.setState({
-        results: [],
-        topResults: topTenish
+        results: [] //clears the pill
       });
+    }
+    for (let result of topTenish) {
+      //extract all the entries from the Dictionary
+      topEntries.push({
+        word: result.searchResult,
+        entry: Dictionary[result.searchResult]
+      });
+    }
+    if (this.state.searchWord !== "") {
+      this.setState({ topResults: topEntries });
+    } else {
+      this.setState({ topResults: [] });
     }
   };
 
--- src/components/results.jsx
+++ src/components/results.jsx
@@ -3,15 +3,21 @@ import React from "react";
 const Result = props => {
   const { searchWord } = props.props;
   const { page } = props.props.results;
+  const { topResults } = props.props;
 
-  return (
-    <ul className="results list-group">
+  let results = [];
+  for (let entry of topResults) {
+    results.push(
       <li className="resultItem list-group-item d-flex justify-content-between align-items-center">
-        {searchWord}
-        <span className="badge badge-primary badge-pill">{page}</span>
+        {entry.word}
+        <span className="badge badge-primary badge-pill">
+          {entry.entry.page}
+        </span>
       </li>
-    </ul>
-  );
+    );
+  }
+
+  return <ul className="results list-group">{results}</ul>;
 };
 
 export default Result;
