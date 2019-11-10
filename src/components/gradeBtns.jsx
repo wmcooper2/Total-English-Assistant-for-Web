a945e8420ac6c867b116eb8382ce9f8ac7244d81
wmc2tokyo <wmcooper2@yahoo.com>
2019-11-10 11:08:47 +0900

    Got the button group to match the grade color (figured out how to override bootstrap stylings). Got the pills to change color based on the grade level, too.

--- src/components/gradeBtns.jsx
+++ src/components/gradeBtns.jsx
@@ -3,6 +3,12 @@ import React from "react";
 const GradeBtn = props => {
   const { handleClick, btnID, text } = props;
   /*const gradeColor = "grade" + btnID + "Color";*/
+  const grade1 = "rgba(0, 189, 128, 1.0)";
+  const grade2 = "rgba(157, 67, 153, 1.0)";
+  const grade3 = "rgba(228, 105, 0, 1.0)";
+  let grade = btnID;
+  let gradeColor = grade === 1 ? grade1 : grade === 2 ? grade2 : grade3;
+  let gradeBorder = "1px solid transparent";
   return (
     <div
       className={"btn btn-primary"}
@@ -10,6 +16,7 @@ const GradeBtn = props => {
         handleClick(event.target.id);
       }}
       id={btnID}
+      style={{ backgroundColor: gradeColor, border: gradeBorder }}
     >
       {text}
     </div>
--- src/components/results.jsx
+++ src/components/results.jsx
@@ -4,9 +4,15 @@ const Result = props => {
   const { topResults, searchWord } = props.props;
   console.log("props = ", props);
 
+  const grade1 = "rgba(0, 189, 128, 1.0)";
+  const grade2 = "rgba(157, 67, 153, 1.0)";
+  const grade3 = "rgba(228, 105, 0, 1.0)";
+
   let results = [];
   if (topResults.length > 1 && searchWord.length > 0) {
     for (let entry of topResults) {
+      let grade = entry.entry.grade;
+      let gradeColor = grade === "1" ? grade1 : grade === "2" ? grade2 : grade3;
       results.push(
         <li
           key={topResults.indexOf(entry)}
@@ -14,7 +20,10 @@ const Result = props => {
         >
           <span>{entry.word}</span>
           <span>{entry.entry.japanese}</span>
-          <span className="badge badge-primary badge-pill">
+          <span
+            className="badge badge-primary badge-pill"
+            style={{ backgroundColor: gradeColor }}
+          >
             {entry.entry.page}
           </span>
         </li>
